ca3fe9c210e95c3dc54b4d537dd171ce94bc4ffa
Neider <napandalesr@gmail.com>
2024-09-19 17:31:57 -0500

    Se agregan los dto al modulo auth

--- src/application/auth/auth.controller.ts
+++ src/application/auth/auth.controller.ts
@@ -1,23 +1,19 @@
 import { Controller, Post, Body, Inject, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(@Inject('authService') private readonly authService: AuthService) {}
 
   @Post('register')
-  async register(
-    @Body('email') email: string,
-    @Body('password') password: string,
-  ) {
-    return await this.authService.register(email, password);
+  async register(@Body() registerDto: RegisterDto) {
+    return await this.authService.register(registerDto.email, registerDto.password);
   }
 
   @Post('login')
-  async login(
-    @Body('email') email: string,
-    @Body('password') password: string,
-  ) {
-    return await this.authService.login(email, password);
+  async login(@Body() loginDto: LoginDto) {
+    return await this.authService.login(loginDto.email, loginDto.password);
   }
 }
