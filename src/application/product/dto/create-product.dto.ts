bb29611909c21fe84bd32220eeaed37de25210b0
Neider <napandalesr@gmail.com>
2024-09-20 08:29:38 -0500

    Se implementa swagger

--- src/application/product/dto/create-product.dto.ts
+++ src/application/product/dto/create-product.dto.ts
@@ -1,19 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateProductDto {
   @IsNumber()
   @IsNotEmpty()
+  @ApiProperty()
   count: number;
 
   @IsNumber()
   @IsNotEmpty()
+  @ApiProperty()
   price: number;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   shipping: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   description: string;
 }
